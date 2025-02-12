d9ca3776555882d231a5b33b9a9e1f22e7164406
yogesh243000 <140988573+yogesh243000@users.noreply.github.com>
2025-02-13 05:07:26 +1100

    register and login setup successfully

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -6,6 +6,7 @@ import { Button } from "../../../components/ui/Button";
 import { Error } from "../../../components/ui/Error"; // Add this import
 import { useRouter } from "next/navigation"; // Import useRouter
 import axiosInstance from "../../../lib/axiosInstance";
+import Link from "next/link";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -47,25 +48,37 @@ const LoginPage = () => {
         <h1 className="text-2xl font-bold text-center">Login Form</h1>{" "}
         {/* Fixed Title */}
         <form onSubmit={handleSubmit} className="mt-6 space-y-4">
-          <Label text="Email" id="email" />
-          <Input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Label text="Password" id="password" />
-          <Input
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div className="flex flex-col gap-2">
+            <Label text="Email" id="email" />
+            <Input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label text="Password" id="password" />
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
           <Error error={error} /> {/* Display error message */}
           <Button type="submit" disabled={isLoading}>
             {isLoading ? "Logging in..." : "Login"}
           </Button>
         </form>
+        <div className="pb-6 mt-4">
+          <p className="text-sm">
+            Don't have an account?{" "}
+            <span className="hover:underline text-blue-500">
+              <Link href="/register">Register</Link>
+            </span>
+          </p>
+        </div>
       </div>
     </main>
   );
--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -5,7 +5,7 @@ import { Input } from "../../../components/ui/Input";
 import { Button } from "../../../components/ui/Button";
 import { Label } from "../../../components/ui/Label";
 import axiosInstance from "../../../lib/axiosInstance";
-
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const RegisterPage = () => {
@@ -49,47 +49,62 @@ const RegisterPage = () => {
   };
 
   return (
-    <main className="min-h-screen w-full flex items-center justify-center bg-blue-400">
-      <div className="flex flex-col justify-center bg-white w-[500px] h-[500px] rounded-md shadow-md p-8">
+    <main className="h-screen flex items-center justify-center bg-blue-400">
+      <div className="flex flex-col justify-center bg-white w-[600px] h-[600px] rounded-md shadow-md p-8">
         <h1 className="text-2xl font-bold text-center">Register Form</h1>
         <form onSubmit={handleSubmit} className="mt-6 space-y-4">
-          <Label text="Name" id="name" />
-          <Input
-            type="text"
-            placeholder="Enter your name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <div className="flex flex-col gap-2">
+            <Label text="Name" id="name" />
+            <Input
+              className="flex flex-col gap-2"
+              type="text"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label text="Email" id="email" />
 
-          <Label text="Email" id="email" />
-          <Input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-          <Label text="Password" id="password" />
-          <Input
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Label text="Confirmed Password" id="password" />
-          <Input
-            type="password"
-            placeholder="Confirm your password"
-            value={confirmedPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-
+            <Input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label text="Password" id="password" />
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label text="Confirmed Password" id="password" />
+            <Input
+              type="password"
+              placeholder="Confirm your password"
+              value={confirmedPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
           <Error error={error} />
 
           <Button type="submit" disabled={isLoading}>
             Register
           </Button>
         </form>
+        <div className="pb-6 mt-4">
+          <p className="text-sm ">
+            Already have an account?{" "}
+            <span className="hover:underline text-blue-500">
+              <Link href="/login">Login</Link>
+            </span>
+          </p>
+        </div>
       </div>
     </main>
   );
